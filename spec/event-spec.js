8ddca685664d75a6a32b66469e6919a5c94d4c98
Rachel Porter <rachelandrewsporter@gmail.com>
2015-12-03 15:12:45 -0500

    installs express and dependencies and includes initial tests

---
+++ spec/event-spec.js
@@ -0,0 +1,39 @@
+describe("an event", function() {
+
+  it("should have a title", function() {
+    
+  });
+
+  it("should have date", function() {
+
+  });
+
+  it("should have a photo url", function() {
+
+  });
+
+  it("should have a wikipedia link", function() {
+
+  });
+
+  it("should have a sub doc of TLDR summaries", function() {
+
+  });
+
+});
+
+describe("a TLDR summary", function() {
+
+  it("should contain a summary with 300 characters or less", function() {
+
+  });
+
+  it("should contain a sentence about why this matters", function() {
+
+  });
+
+  it("should be able to accept up/down votes", function() {
+
+  });
+
+});
