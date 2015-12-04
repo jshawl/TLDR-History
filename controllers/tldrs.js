e94fb36de655cc065575d27433e52536eed455b8
Tory Burgett <vdburgett@gmail.com>
2015-12-04 15:06:11 -0500

    Controllers for eventPosts and tldrs, include  get post patch delete

---
+++ controllers/tldrs.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+var EventPost = require("../models/eventPost");
+var Tldr = require("../models/tldr");
+
+function error(response, message){
+  response.status(500);
+  response.json({error: message});
+}
+
+router.get("/", function(req, res){
+  Tldr.find({}).populate("eventPost", "title").then(function(tldrs){
+    res.json(tldrs);
+  });
+});
+
+router.get("/:id", function(req, res){
+  Tldr.findById(req.params.id).populate("eventPost", "title").then(function(tldr){
+    res.json(tldr);
+  });
+});
+
+router.put("/:id", function(req, res){
+  Tldr.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true}).then(function(tldr){
+    res.json(tldr);
+  });
+});
+
+router.delete("/:id", function(req, res){
+  Tldr.findById(req.params.id).then(function(tldr){
+    EventPost.findByIdAndUpdate(tldr.eventPost._id, {
+      $pull: { tldrs: {_id: req.params.id} }
+    }).then(function(){
+      return tldr.remove();
+    }).then(function(){
+      res.json({success: true});
+    });
+  });
+});
+
+module.exports = router;
