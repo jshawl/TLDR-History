81ce8def3fb37d3cd284cd3b044e14550aabcd4a
Rachel Porter <rachelandrewsporter@gmail.com>
2015-12-07 13:19:37 -0500

    adds seeds placeholders

--- db/seeds.js
+++ db/seeds.js
@@ -8,3 +8,11 @@ EventPostModel.remove({}, function(err){
 TldrModel.remove({}, function(err){
   console.log(err)
 })
+
+var event1 = new EventPostModel({title: "[EVENT TITLE1]"})
+var event2 = new EventPostModel({title: "[EVENT TITLE2]"})
+var event3 = new EventPostModel({title: "[EVENT TITLE3]"})
+
+var tldr1 = new TldrModel({body: "TLDR1!!"})
+var tldr2 = new TldrModel({body: "TLDR2!!"})
+var tldr3 = new TldrModel({body: "TLDR3!!"})
