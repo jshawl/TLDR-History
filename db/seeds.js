94eb9a61241f1f7dc90b960a276082c70ca6b718
Rachel Porter <rachelandrewsporter@gmail.com>
2015-12-08 09:02:20 -0500

    adds new seeds and as-yet-non-working tests

--- db/seeds.js
+++ db/seeds.js
@@ -32,23 +32,32 @@ var event3 = new EventPostModel( {
   })
   event3.save();
 
+var event4 = new EventPostModel({
+  title: "Pearl Harbor Day",
+  date: "December 7, 1941",
+  photoUrl: "https://40.media.tumblr.com/6375b044364724a590305494fa56562d/tumblr_mh1rj198rZ1rmzffko1_400.jpg",
+  wikiPage: "https://en.wikipedia.org/wiki/Attack_on_Pearl_Harbor"
+}).save();
 
 var tldr1 = new TldrModel({
       "summary": "America's birthday. The Continental Congress got together and sent the King George III the signed Decloration of Independence.",
       "relevance": "America's birthday/excuse for bbq parties and fireworks"
-    })
-    tldr1.save();
+    }).save();
+
 var tldr2 = new TldrModel({
       "summary": "The founding fathers send a breakup letter to the British Empire via the Decloration of Independence.",
       "relevance": "FIREWORKS DAY in America, also America's anniversery."
-    })
-    tldr2.save();
+    }).save();
+
 var tldr3 = new TldrModel({
       "summary": "AKA Independence Day - Day when Continental Congress got together and signed the Declaration of Independence.",
       "relevance": "Day America split up from the British Empire."
-    })
-    tldr3.save();
+    }).save();
 
+var tldr4 = new TldrModel({
+  summary: "Japan was trying to get resources from Southeast Asia and the US said hell no, if you go attack Southeast Asia well attack you! So they thought they could catch us with our pants down and decimate our fleet at Pearl Harbor and Then go get the resources they needed in Indonesia and other places.",
+  relevance: "This event provoked the US to enter World War II AND long story short we dropped atom bombs on Japan http://bit.ly/1Tyl5vh  #tldrhistory"
+}).save();
 
-event1.tldrs.push(tldr1, tldr2, tldr3)
-event1.save();
+event1.tldrs.push(tldr1, tldr2, tldr3).save();
+event4.tldrs.push(tldr4).save();
--- spec/eventPost-spec.js
+++ spec/eventPost-spec.js
@@ -1,18 +1,33 @@
 var request = require('request');
+var EventPostModel = require("../models/eventPost")
+var TldrModel = require("../models/tldr")
 
-describe("an event", function() {
 
-  it("should have a title", function(next) {
+
+describe("adding a new event", function() {
+  var post;
+  beforeEach(function() {
+    EventPostModel.remove({}, function(err){
+      console.log(err)
+    })
+    TldrModel.remove({}, function(err){
+      console.log(err)
+    })
+
     request.post("http://localhost:3000/eventPosts",{
       form: {
-        title: "my post title"
-
+        title: "my post title",
+        date: "July 4, 1776",
+        photoUrl: "http://someimage.com",
+        wikiPage: "http://en.wikipedia.org/wiki/Independence_Day_(United_States)"
       }
     }, function(err, res, body){
-      var post = JSON.parse(body);
-      expect(post.title).toEqual("my post title");
-      next();
+      post = JSON.parse(body);
     })
+  })
+
+  it("should have a title", function() {
+    expect(post.title).toEqual("my post title");
   });
 
   it("should have date", function() {
