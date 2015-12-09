35e9ed21b43bb2436b5302c76de82b3e2448950e
Rachel Porter <rachelandrewsporter@gmail.com>
2015-12-09 16:51:12 -0500

    temp tldrview changes

--- public/js/views/tldrView.js
+++ public/js/views/tldrView.js
@@ -1,6 +1,23 @@
 var TldrView = function(tldr){
   this.tldr = tldr;
+  this.$el = $("<div class='tldr well'></div>");
+  this.render();
+
+  $(".tldrs").append(this.$el); //how to append to only its parent div? this.eventPost?
 };
+
+// when the TLDRview is rendered (eventPostView.prototype), there should be buttons to edit, delete or create new
+// edit button should render edit template,
+// delete button should delete tldr and fade out
+// create new button should render new form, which has "create new" and "cancel" buttons
+    // eventually this should be a toggle function rather than hiding create button and adding cancel button
+
+// should go on EventPostView:
+    //rachel changes begin
+    var editButton = this.$el.find(".editTldr");
+    var deleteButton = this.$el.find(".deleteTldr");
+    // need to add click listeners
+    // rachel changes end
 
 TldrView.prototype = {
   render: function(){
