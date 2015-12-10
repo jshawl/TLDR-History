dfc4ad888f40036473552dc1c933c8556f5fb4f6
Dumitru Gorgan <dgorgan3@gmail.com>
2015-12-10 12:34:37 -0500

    added twitter feature for users to share their tldrs on twitter and styled the tweet this button

--- public/js/views/tldrView.js
+++ public/js/views/tldrView.js
@@ -5,7 +5,13 @@ var TldrView = function(tldr){
 TldrView.prototype = {
   render: function(){
     var self = this;
-    var $el = $("<div class='tldrDiv well'><p class='tldrSummary'>" + self.tldr.summary + "</p><p class='tldrRelevance'>"+ self.tldr.relevance +"</p></div>");
+
+    //encoded the url and tldr.relevance for use in twitter link
+    var encodedUrl = encodeURIComponent(self.tldr.relevance);
+    var url = encodeURIComponent('tldr%2Dhistory.herokuapp.com');
+
+    console.log(this);
+    var $el = $("<div class='tldrDiv well'><p class='tldrSummary'>" + self.tldr.summary + "</p><p class='tldrRelevance'>" + self.tldr.relevance +   "</p>" + '<a href="http://www.twitter.com/share?url=tldr-history.herokuapp.com&text=' + encodedUrl + " https://goo.gl/l3sZyi %23tldrhistory"+'"class="btn btn-info twitter"">' + 'Tweet this! '+ '<span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span>' + '</a>' + "</div>");
     return($el);
   }
 };
