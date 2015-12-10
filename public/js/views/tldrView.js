b26347755c43ed0e0cc996ea37368c0b3a97d695
Dumitru Gorgan <dgorgan3@gmail.com>
2015-12-10 14:17:29 -0500

    added mention instead of hashtag

--- public/js/views/tldrView.js
+++ public/js/views/tldrView.js
@@ -11,7 +11,7 @@ TldrView.prototype = {
     var url = encodeURIComponent('tldr%2Dhistory.herokuapp.com');
 
     console.log(this);
-    var $el = $("<div class='tldrDiv well'><p class='tldrSummary'>" + self.tldr.summary + "</p><p class='tldrRelevance'>" + self.tldr.relevance +   "</p>" + '<a href="http://www.twitter.com/share?url=tldr-history.herokuapp.com&text=' + encodedUrl + " https://goo.gl/l3sZyi %23tldrhistory"+'"class="btn btn-info twitter"">' + 'Tweet this! '+ '<span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span>' + '</a>' + "</div>");
+    var $el = $("<div class='tldrDiv well'><p class='tldrSummary'>" + self.tldr.summary + "</p><p class='tldrRelevance'>" + self.tldr.relevance +   "</p>" + '<a href="http://www.twitter.com/share?url=tldr-history.herokuapp.com&text=' + encodedUrl + 'https://goo.gl/l3sZyi %40tldrhistory" class="btn btn-info twitter">Tweet this!<span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span></a></div>');
     return($el);
   }
 };
